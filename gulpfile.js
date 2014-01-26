4763a3595b7a71f6a66998247f234e1e9faa6b7f
Mikko Lehtinen <mikko+gh@applifier.com>
2014-01-26 18:00:20 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+
+gulp.task('styl', function () {
+  gulp.src('./src/styles/*.styl')
+    .pipe(stylus({use: ['nib']}))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/**' , function(ev){
+    gulp.run('styl')
+  });
+});
